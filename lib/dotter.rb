799f9194bc650cb3a5a79678a555e7e67fe85bbc
Piotr Sarnacki <drogus@gmail.com>
2009-10-20 10:56:13 +0200

    Some more Documantation

--- lib/dotter.rb
+++ lib/dotter.rb
@@ -2,9 +2,31 @@ require "RMagick"
 
 module Dotter
 end
+
+# Dotter::Dotter allows to generate transparent image with dots representing points on map. This image can be used as a google maps tile. 
+#
+# Usage:
+#
+#     dotter = Dotter::Dotter.new(:dots => dots_array)
+#
+#     image = dotter.generate_image
+#     image # Magick::Image instance containing transparent image with dots
+#
+# A Dot can be any object that responds to :x and :y methods. If :colorize in options is set to true and dot responds to :dot_color method, dot will be drawn with this color (color can be anything that is acceptable as a color in RMagick, usually RGB value, ie: "#ff0000". Dots are black by default)
+#
+# x and y coordinates represents the actual place, where the dot will be placed on image. If you have latitude and longitude coordinates you must convert it first. It's fairly easy with Dotter::Tile, which abstracts google maps tiles.
 class Dotter::Dotter
   attr_accessor :dots, :colorize
 
+  # ==== Parameters
+  # options<Hash>::
+  #
+  # ==== Options
+  # :dots<Enumerable>:: array of dots that should be displayed on the image
+  # :width<Fixnum>:: tile width (default: 256)
+  # :height<Fixnum>:: tile height (default: 256)
+  # :radius<Fixnym>:: dot radius (default: 5)
+  # :colorize<Boolean>:: if colorize is set to true and dot responds_to dot_color, dot is colored using that color. Otherwise it's black (default: true)
   def initialize(opts = {})
     @dots = opts.delete(:dots) || []
     @width = opts.delete(:width) || 256
@@ -14,10 +36,12 @@ class Dotter::Dotter
     @colorize = colorize.nil? ? true : colorize
   end
 
+  # Tells if dots can be colored. If yes, dotter colors dot with anything that is returned by dot_color method.
   def colorize?
     @colorize
   end
 
+  # Generate transparent image with dots
   def generate_image
     image = Magick::Image.new(@width, @height) do
       self.background_color = "transparent"
@@ -28,7 +52,8 @@ class Dotter::Dotter
     image
   end
 
-  def draw_dots(image)
+private
+  def draw_dots(image) #:nodoc
     return if dots.nil? || dots.length == 0
     gc = Magick::Draw.new
 
--- lib/dotter/tile.rb
+++ lib/dotter/tile.rb
@@ -1,6 +1,24 @@
-# class abstracting google maps' tiles (squares that are loaded as parts of the map)
-# more on tiles and how google maps works can be found here:
-# http://code.google.com/apis/maps/documentation/overlays.html#Google_Maps_Coordinates
+# Dotter::Tile is abstracting google maps' tiles (squares, usually 256x256, that are loaded as parts of the map).
+# 
+# More on tiles and how google maps works can be found here: http://code.google.com/apis/maps/documentation/overlays.html#Google_Maps_Coordinates
+#
+# ==== Usage
+#
+# To generate a tile you must give upper left corner of a tile and zoom.
+#
+#     tile_start = Dotter::LatLng.new(tile_start_latitude, tile_start_longitude)
+#     tile = Dotter::Tile.new(tile_start, zoom)
+#
+#     tile.places = places_array # places should respond_to latitude and longitude
+#
+#     tile.image # generates RMagick::Magick image with "png" format set
+#
+# You can also use tile in Dotter::Dotter explicitly (tile.image is a simple helper, but if you want to set some specific options you will have to do that this way):
+#     dotter = Dotter::Dotter.new(:dots => tile.places_as_points)
+#     dotter.generate_image
+#
+# It passes places changed into points relative to current tile's start.
+#
 class Dotter::Tile
   attr_accessor :zoom, :start, :places, :dots
   def initialize(latlng, zoom)
@@ -8,8 +26,10 @@ class Dotter::Tile
     @start = Dotter::GMap.latlng_to_pixel(latlng, @zoom)
   end
 
-  # method that takes collection of objects responding to latitude and longitude
-  # methods and converting it to points on current tile
+  # Method that takes places passed to Tile and converts them into points with x an y relative to tile start
+  # 
+  # ==== Returns
+  # Array[~x, ~y]:: points array
   def places_as_points
     points = []
     places.each do |place|
@@ -25,7 +45,7 @@ class Dotter::Tile
     points
   end
 
-  # generate tile image with Dotter
+  # Generate tile image with Dotter
   def image
     dotter = Dotter::Dotter.new(:dots => @dots || places_as_points)
     img = dotter.generate_image
