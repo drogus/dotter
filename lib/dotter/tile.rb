89548e8e82f648e38beeac2e16be171f19461964
Piotr Sarnacki <drogus@gmail.com>
2009-11-02 18:03:46 +0100

    Some methods in dotter has been changed

--- lib/dotter/tile.rb
+++ lib/dotter/tile.rb
@@ -9,36 +9,36 @@
 #     tile_start = Dotter::LatLng.new(tile_start_latitude, tile_start_longitude)
 #     tile = Dotter::Tile.new(tile_start, zoom)
 #
-#     tile.places = places_array # places should respond_to latitude and longitude
+#     tile.locations = locations_array # locations should respond_to latitude and longitude
 #
 #     tile.image # generates RMagick::Magick image with "png" format set
 #
 # You can also use tile in Dotter::Dotter explicitly (tile.image is a simple helper, but if you want to set some specific options you will have to do that this way):
-#     dots = tile.convert_places
+#     dots = tile.generate_xy_coordinates!
 #     dotter = Dotter::Dotter.new(:dots => dots)
 #     dotter.generate_image
 #
-# It passes places changed into points relative to current tile's start.
+# It passes locations changed into points relative to current tile's start.
 #
 class Dotter::Tile
-  attr_accessor :zoom, :start, :places, :dots
+  attr_accessor :zoom, :start, :locations, :dots
   def initialize(latlng, zoom)
     @zoom = zoom.to_i
     @start = Dotter::GMap.latlng_to_pixel(latlng, @zoom)
   end
 
-  # Method that takes places passed to Tile and converts them into points with x an y relative to tile start
+  # Method that takes loactions passed to Tile and converts them into points with x an y relative to tile start
   # 
   # ==== Returns
   # Array[~x, ~y]:: points array
-  def places_as_points(options = {})
+  def locations_as_points(options = {})
     points = []
-    places.each do |place|
-      latlng = Dotter::LatLng.new(place.latitude, place.longitude)
+    locations.each do |location|
+      latlng = Dotter::LatLng.new(location.latitude, location.longitude)
       point = Dotter::GMap.latlng_to_pixel(latlng, zoom)
-      point = point - start # place is relative to big map, let's make it relative to start of tile
+      point = point - start # location is relative to big map, let's make it relative to start of tile
       if block_given?
-        yield(point, place)
+        yield(point, location)
       else
         points << point
       end
@@ -46,20 +46,19 @@ class Dotter::Tile
     points
   end
 
-  # Method converts places to points if places responds to x= and y=
+  # Method sets x and y coordinates for each place
   #
-  def convert_places
-    places_as_points do |point, place|
-      place.x = point.x
-      place.y = point.y
+  def generate_xy_coordinates!
+    locations_as_points do |point, location|
+      location.x = point.x
+      location.y = point.y
     end
-    @dots = places
-    places
+    @dots = locations
   end
 
   # Generate tile image with Dotter
   def image
-    dotter = Dotter::Dotter.new(:dots => @dots || places_as_points)
+    dotter = Dotter::Dotter.new(:dots => @dots || locations_as_points)
     img = dotter.generate_image
     img.format = "png"
     img
--- spec/dotter/tile_spec.rb
+++ spec/dotter/tile_spec.rb
@@ -12,7 +12,7 @@ describe "Dotter::Tile" do
     # i know that this isn't good spec, it's just to run code and see it if not fails
     # need to add more specs fot Tile
     tile = Dotter::Tile.new(Dotter::LatLng.new(0, 0), 0)
-    tile.places = [Dotter::LatLng.new(0, 0)]
+    tile.locations = [Dotter::LatLng.new(0, 0)]
     tile.image.class.should == Magick::Image
     lambda { tile.image.to_blob }.should_not raise_error
   end
@@ -21,11 +21,11 @@ describe "Dotter::Tile" do
     before(:each) do
       @tile = Dotter::Tile.new(Dotter::LatLng.new(0, 0), 1)
       @places = [Dotter::LatLng.new(0, 0), Dotter::LatLng.new(-85.1, 180)]
-      @tile.places = @places
+      @tile.locations = @places
     end
 
     it "should convert places to points" do
-      points = @tile.places_as_points
+      points = @tile.locations_as_points
       points.should have(2).items
       points.first.x.should == 0
       points.first.y.should == 0
@@ -34,10 +34,10 @@ describe "Dotter::Tile" do
     end
 
     it "should set x and y in current points if points responds_to x= and x=" do
-      stub_point(@tile.places[0], 0, 0)
-      stub_point(@tile.places[1], 111, 111)
+      stub_point(@tile.locations[0], 0, 0)
+      stub_point(@tile.locations[1], 111, 111)
 
-      @tile.convert_places
+      @tile.generate_xy_coordinates!
     end
   end
 
